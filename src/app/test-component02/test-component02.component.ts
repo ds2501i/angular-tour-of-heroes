08eab58bf2988b17c916639be1e61e4befc0cca7
ds2501i <ds2501i@checkboxtechnology.com>
2025-01-19 14:22:01 +0530

    test

---
+++ src/app/test-component02/test-component02.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-component02',
+  templateUrl: './test-component02.component.html',
+  styleUrls: ['./test-component02.component.css']
+})
+export class TestComponent02Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
